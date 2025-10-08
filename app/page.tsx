bef1f954f055ef8e3d1b37bd4c4827c4d701d207
Dave Donnelly <davepauldonnelly@gmail.com>
2025-10-08 23:44:48 +1100

    Removal of GitHub calendar package

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import { MicroBio } from '../components/home/MicroBio';
 import { SignatureProjects } from '../components/home/SignatureProjects';
 import { WhyHireMe } from '../components/home/WhyHireMe';
 import { AnimatedBackground } from '../components/shared/AnimatedBackground';
-import { generateMetadata as generateSEOMetadata } from '../lib/seo';
 import { microBioData } from '../data/microBio';
 import { projects } from '../data/projects';
+import { generateMetadata as generateSEOMetadata } from '../lib/seo';
 
 export const metadata: Metadata = generateSEOMetadata({
   title: 'Dave Donnelly - Web Developer',
